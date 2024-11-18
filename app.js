98196fa7ec37d56804ff675e5914bea9ed6d817f
allvintageisak <mwangasuisack@gmail.com>
2024-11-18 23:17:48 +0300

    working on active btn

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections= document.querySelector('.main-content');
 
 function pageTransitions(){
-    
+    //button click active class
+   for(let i = 0; i < sectBtn.length; i++){
+    sectBtn[i].addEventListener('click', () =>{
+        let currentBtn = document.querySelectorAll('.active-btn');
+        currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
+        this.className += 'active-btn'
+    })
+   } 
 }
+
+pageTransitions();
